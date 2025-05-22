9b00f6b4f0a76a290de2091ffe96f31bb4329d83
Adeliaswa <adeliaswastikadewi@gmail.com>
2025-05-22 19:51:30 +0700

    Update HapusDirektori.java

--- HapusDirektori.java
+++ HapusDirektori.java
@@ -2,20 +2,20 @@ import java.io.File;
 
 public class HapusDirektori {
     public static void main(String[] args) {
-        var folder = new File("cekhapusdirektori"); // ganti "data" dengan nama folder kamu
+        var folder = new File("cekhapusdirektori"); //folder yang saya gunakan untuk mengecek kode yaitu menghapus direktorinya
 
-        if (folder.exists() && folder.isDirectory()) {
-            File[] files = folder.listFiles();
+        if (folder.exists() && folder.isDirectory()) { //cek apakah folder ada dan benar-benar direktori
+            File[] files = folder.listFiles(); //mengambil semua file yang ada di direktori tersebut
 
-            if (files != null) {
-                for (File file : files) {
-                    if (file.isFile()) {
-                        file.delete(); // hapus file
+            if (files != null) { //memastikan apakah daftar file tidak null (folder bisa diakses)
+                for (File file : files) { 
+                    if (file.isFile()) { //cek apakah file yang diakses adalah file biasa bukan direktori
+                        file.delete(); //hapus file
                     }
                 }
             }
 
-            boolean success = folder.delete(); // hapus direktori setelah kosong
+            boolean success = folder.delete(); //hapus direktori setelah kosong/ tidak ada file lagi
             System.out.println(success ? "Direktori berhasil dihapus" : "Gagal menghapus direktori");
         } else {
             System.out.println("Direktori tidak ditemukan.");
