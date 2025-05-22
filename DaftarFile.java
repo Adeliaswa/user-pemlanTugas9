95bc174c4aa2e3e970c034d2b3de83733ff12341
Adeliaswa <adeliaswastikadewi@gmail.com>
2025-05-22 19:51:57 +0700

    Update DaftarFile.java

--- DaftarFile.java
+++ DaftarFile.java
@@ -4,16 +4,16 @@ public class DaftarFile {
     public static void main(String[] args) {
         var folder = new File("."); //direktori saat ini
 
-        if (folder.exists() && folder.isDirectory()) {
-            File[] files = folder.listFiles();
-            if (files != null) {
-                for (File f : files) {
-                    if (f.isFile()) {
-                        System.out.println(f.getName());
+        if (folder.exists() && folder.isDirectory()) { //mengecek apakah folder ada dan merupakan direktori
+            File[] files = folder.listFiles(); //mengambil semua file dan folder di dalam direktori
+            if (files != null) { //mengecek apakah hasilnya tidak null (berarti bisa dibaca)
+                for (File f : files) { //mengulang setiap file/folder
+                    if (f.isFile()) { //mengecek apakah file/folder merupakan file (bukan folder)
+                        System.out.println(f.getName()); 
                     }
                 }
             }
-        } else {
+        } else { //jika folder tidak ada atau bukan direktori
             System.out.println("Direktori tidak valid.");
         }
     }
